561326f2ab3025492bdd8043373a277ecc79cf79
YE Xiaotong <1155219812@link.cuhk.edu.hk>
2025-01-20 22:52:56 +0800

    my perfect answer

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ For this question, letters are NOT case-sensitive, for example, "LEVeL" is a pal
 """
 # Change this function so it works correctly
 def is_palindrome(text):
-    return False
+    compare_text = text.replace(' ','').lower()
+    compare_text = ''.join([l for l in compare_text if l.isalnum()])
+    return compare_text == compare_text[::-1]
 
 
 if __name__ == '__main__':
