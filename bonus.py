cae47befff017aab8d9ab589b08a8eff1ce4a9cd
YE Xiaotong <1155219812@link.cuhk.edu.hk>
2025-01-26 12:57:25 +0800

    my perfect answer

--- bonus.py
+++ bonus.py
@@ -8,7 +8,20 @@ For this question, letters are case-sensitive, for example, "Aa" is not consider
 """
 # Change this function so it works correctly
 def longest_palindrome_length(text):
-    return 0
+    from collections import Counter
+    chr_count=Counter(text)
+    length=0
+    odd_found=False
+    for count in chr_count.values():
+        if count%2==0:
+            length+=count
+        else:
+            length+=count-1
+            odd_found=True
+    if odd_found:
+        length+=1
+    return length
+
 
 
 if __name__ == '__main__':
